8d0547cd06acb36bd7d79cfdb3c4d50a29eb2bad
Maneren <49210777+Maneren@users.noreply.github.com>
2024-01-01 01:13:31 +0100

    ignore empty iterators

--- player.py
+++ player.py
@@ -398,10 +398,10 @@ class Player(Board):
             if not self.hive:
                 return Move(Piece.Spider, None, (3, 6)).to_brute(self.upper)
 
-            placement = random.choice([*self.cells_around_hive])
+            placement = random.choice(list(self.cells_around_hive))
             return Move(Piece.Spider, None, placement).to_brute(self.upper)
 
-        possible_moves = [*self.valid_moves]
+        possible_moves = list(self.valid_moves)
 
         if not possible_moves:
             return []
